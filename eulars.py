5268b5289458da266be16a303c740ac10a26ee89
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-12-17 16:18:41 +0530

    eulars method

---
+++ eulars.py
@@ -0,0 +1,16 @@
+class Euler:
+    def __init__(h):
+        self.h = h
+    
+
+    def numericalMethod(n):
+        pass
+    
+    def solveEquation(position, firstValue):
+        print('Hello WOrld')
+
+
+
+
+if(__name__ == '__main__'):
+    eular = Eular(2)
